bbdaf8d0c4fa8c680d2c5726ac6ff57b0cf38524
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2023-03-25 23:04:42 +0800

    enunux: migrate to numsharp

---
+++ OpenUtau.Plugin.Builtin/EnunuOnnx/F0.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using NumSharp;
+
+//reference: https://github.com/r9y9/nnmnkwii/blob/master/nnmnkwii/preprocessing/f0.py
+
+namespace OpenUtau.Plugin.Builtin.EnunuOnnx.nnmnkwii.preprocessing{
+    public static class f0{
+        public static NDArray interp1d(NDArray f0){
+            int ndim = f0.ndim;
+            //if len(f0) != f0.size:
+            //    raise RuntimeError("1d array is only supported")
+            var continuous_f0 = f0.flatten();
+            var nonzero_indices = np.nonzero(continuous_f0)[0].ToArray<int>();
+            
+            //Nothing to do
+            if(nonzero_indices.Length<=0){
+                return f0;
+            }
+
+            //Need this to insert continuous values for the first/end silence segments
+            continuous_f0[0] = continuous_f0[nonzero_indices[0]];
+            continuous_f0[-1] = continuous_f0[nonzero_indices[^1]];
+
+            //interpolate
+            Enumerable.Zip(
+                nonzero_indices.Prepend(0),
+                nonzero_indices.Append(continuous_f0.size-1),
+                (a,b)=>{
+                    if(b-a>1){
+                        continuous_f0[new Slice(a,b+1)] = np.linspace(continuous_f0[a],continuous_f0[b],b-a+1);
+                    }
+                    return true;
+                }
+            ).Last();
+            return continuous_f0;
+        }
+    }
+}
